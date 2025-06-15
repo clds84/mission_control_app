57ec424826466a176ec260040b21f64eb6f67533
cristian <clds.1984@gmail.com>
2025-06-15 05:41:56 +0100

    fix time.sleep and uncomment logs

--- battery_status.py
+++ battery_status.py
@@ -59,61 +59,60 @@ def battery_life_thread():
                 battery_life = get_battery_life()
                 sio.emit('battery_update_mc', battery_life)
         except Exception as e:
-            # logging.info("[bat_gauge] Error sending battery data: {e}")
-        time.sleep(1)
+            logging.info("[bat_gauge] Error sending battery data: {e}")
+            time.sleep(1)
 
 def read_serial_forever():
-    # logging.info("[bat_gauge] Serial read thread started")
+    logging.info("[bat_gauge] Serial read thread started")
     while True:
         try:
             if sio_neo.connected:
                 raw = ser.readline()
-                # logging.info(f"Raw bytes: {raw}")
                 line = raw.decode().strip()
-                # logging.info(f"Decoded line: '{line}'")
 
                 if line.startswith("led_encoder_button_state:"):
                     led_encoder_button_state = line.split(":")[1].split(",")[0].strip()
                     logging.info(f"Encoder state received: {led_encoder_button_state}")
                     sio_neo.emit('led_encoder_button_state', led_encoder_button_state)
         except Exception as e:
-            # logging.info("[bat_gauge] Error reading serial: {e}")
-        time.sleep(0.1)
+            logging.info("[bat_gauge] Error reading serial: {e}")
+            pass
+            time.sleep(0.1)
 
 @sio.event
 def connect():
-    # logging.info("[bat_gauge] Connected to robot server")
+    logging.info("[bat_gauge] Connected to robot server")
 @sio_neo.event
 def connect():
-    # logging.info("Mission Control Connected to NeoPixel server")
+    logging.info("Mission Control Connected to NeoPixel server")
 
 @sio.event
 def disconnect():
-    # logging.info("[bat_gauge] Disconnected from robot server")
+    logging.info("[bat_gauge] Disconnected from robot server")
 @sio_neo.event
 def disconnect():
-    # logging.info("Missoin Control Disconnected from NeoPixel server")
+    logging.info("Missoin Control Disconnected from NeoPixel server")
 
 
 def connect_to_robot_with_retry():
     while True:
         try:
-            # logging.info("[bat_gauge] Attempting to connect to robot server...")
+            logging.info("[bat_gauge] Attempting to connect to robot server...")
             sio.connect('http://headless4.local:8000')
             break  # If connected, exit loop
         except Exception as e:
-            # logging.info("Connection failed: {e}")
+            logging.info("Connection failed: {e}")
             time.sleep(.5)
 
 def connect_to_NeoPixel_with_retry():
     while True:
         try:
-            # logging.info("Mission Control Attempting to connect to NeoPixel server...")
+            logging.info("Mission Control Attempting to connect to NeoPixel server...")
             sio_neo.connect('http://headless4.local:9000')
             break  # If connected, exit loop
         except Exception as e:
-            # logging.info("Connection failed: {e}")
-            # time.sleep(.5)
+            logging.info("Connection failed: {e}")
+            time.sleep(.5)
 
 if __name__ == '__main__':
     try:
@@ -125,6 +124,6 @@ if __name__ == '__main__':
         connect_to_NeoPixel_with_retry() 
         battery_life_thread()
     except KeyboardInterrupt:
-        # logging.info("Mission Control stopped by user")
+        logging.info("Mission Control stopped by user")
         sio.disconnect()
         sio_neo.disconnect()
